02f554089dfcf90ecb72cb5aa3dc764dec5ea26c
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-10-26 14:47:47 +0530

    fetch data and loading state

--- books-library/src/App.js
+++ books-library/src/App.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://openlibrary.org/search.json?author=tolkien")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .then(() => setLoading())
+      .catch(setError);
+  }, []);
+
+  if (loading) {
+    return <h1 style={{ textAlign: "center" }}>Loading...</h1>;
+  }
+  if (error) {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  }
+  if (data) {
+    return <div className="container">{data.docs[0].title}</div>;
+  }
+
+  return <h1>Hello</h1>;
 }
 
 export default App;
